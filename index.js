09f19b08bff8e0fcc57a1da9989956a31c48127c
Davron <104767260+davron-m@users.noreply.github.com>
2023-03-08 16:56:07 +0300

    added

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const { Console } = require('console')
 const express = require('express')
-
 const app = express()
 
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+
+/*app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+})*/
+
+
 app.get('/', (req, res) =>{
-    res.send('This homepage')
+    res.render('index')
 })
 
 app.get('/about', (req, res) =>{
     res.send('About us')
 })
 
-app.get('/user/:username/:id', (req, res) =>{
+/*app.get('/user/:username/:id', (req, res) =>{
     res.send(`User ID: ${req.params.id}. Username; ${req.params.username}`)
+})*/
+
+app.get('/user/:username', (req, res) => {
+    let data = {username: req.params.username, hobbies: ['Football', 'Skate', 'Hockey']}
+    res.render('user', data)
 })
 
 
